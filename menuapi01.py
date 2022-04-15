5a2b87d865d0b65c3bfcf6b75774b98c9bb80780
Devin Sith <devinsith.dev@gmail.com>
2022-04-14 23:12:21 -0700

    added files

---
+++ menuapi01.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""DEMO: receiving JSON"""
+
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import jsonify
+
+app = Flask(__name__)
+
+menu = [{
+        "appetizers": [
+            {
+                "name": "Pork Gyoza",
+                "price": 10,
+            },
+            {
+                "name": "Edamame",
+                "price": 4,
+            },
+            {
+                "name": "Chicken Karaage",
+                "price": 8,
+            },
+            {
+                "name": "Takoyaki",
+                "price": 7,
+            }
+        ],
+        "main": [
+            {
+                "name": "Tonkotsu Ramen",
+                "price": 15,
+            },
+            {
+                "name": "Miso Ramen",
+                "price": 15,
+            },
+            {
+                "name": "Chashu Bowl",
+                "price": 6,
+            },
+            {
+                "name": "Karaage Bowl",
+                "price": 6,
+            }
+        ],
+        "drinks": [
+            {
+                "name": "Green Tea",
+                "price": 2,
+            },
+            {
+                "name": "Sapporo",
+                "price": 5,
+            }
+        ]
+        }]
+
+
+@app.route("/")
+def index():
+    # jsonify returns legal JSON
+    return jsonify(menu)
+
+
+if __name__ == "__main__":
+    app.run(port=3000)
---
+++ menurequest01.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import requests
+from pprint import pprint
+from flask import Flask
+from flask import session
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import escape
+from flask import request
+from datetime import date
+
+
+app = Flask(__name__)
+app.secret_key = "asdsa456564dasdsd"
+
+
+@app.route("/")
+def index():
+    # if the key "username" has a value in session
+    if "username" in session:
+        username = session["username"]
+        URL = "http://127.0.0.1:3000/"
+
+        menu = requests.get(URL).json()
+
+        return render_template("menu.html", name=username, menu=menu, date=date.today())
+
+    return render_template("splash.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # if you sent us a POST because you clicked the login button
+    if request.method == "POST":
+
+        # request.form["xyzkey"]: use indexing if you know the key exists
+        # request.form.get("xyzkey"): use get if the key might not exist
+        session["username"] = request.form.get("username")
+        return redirect(url_for("index"))
+
+    # return this HTML data if you send us a GET
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove the username from the session if it is there
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
